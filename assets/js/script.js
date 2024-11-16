d47e861454acb1c8a95e9e2dfd71b2c907a946ca
yatharth1999 <yatharth.vohra@gmail.com>
2024-11-16 01:46:09 -0800

    made some ui changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,25 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
 
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
-
-window.onscroll = function () {
-    const backToTopButton = document.querySelector('.back-to-top');
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        backToTopButton.style.display = 'block';
-    } else {
-        backToTopButton.style.display = 'none';
-    }
-};
-
-// Function to scroll to the top
-function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
     });
-}
 
+    window.onscroll = function () {
+        const backToTopButton = document.querySelector('.back-to-top');
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            backToTopButton.style.display = 'block';
+        } else {
+            backToTopButton.style.display = 'none';
+        }
+    };
 
+    // Function to scroll to the top
+    function scrollToTop() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
+});
