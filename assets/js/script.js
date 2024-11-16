a9582042ba77e0ce5a2777c240b82915fb228d76
yatharth1999 <yatharth.vohra@gmail.com>
2024-11-16 01:31:18 -0800

    more_chnages

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+window.onscroll = function () {
+    const backToTopButton = document.querySelector('.back-to-top');
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopButton.style.display = 'block';
+    } else {
+        backToTopButton.style.display = 'none';
+    }
+};
+
+// Function to scroll to the top
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+
